c4eab24bdf903e5c69e92038a8cb36f693294273
LisaTan <50634813+LisaTOVO@users.noreply.github.com>
2020-04-24 00:04:06 -0400

    .

---
+++ month_to_days.py
@@ -0,0 +1,40 @@
+m = str(int(input("Enter the number of the month: ")))
+a = "1"
+b = "2"
+c = "3"
+d = "4"
+e = "5"
+f = "6"
+g = "7"
+h = "8"
+i = "9"
+j = "10"
+k = "11"
+l = "12"
+
+if m == a:
+    print("January, 31 days")
+elif m == b:
+    print("February, 28 or 29 days")
+elif m == c:
+    print("March, 31 days")
+elif m == d:
+    print("April, 30 days")
+elif m == e:
+    print("May, 31 days")
+elif m == f:
+    print("June, 30 days")
+elif m == g:
+    print("July, 31 days")
+elif m == h:
+    print("August, 31 days")
+elif m == i:
+    print("September, 30 days")
+elif m == j:
+    print("October, 31 days")
+elif m == k:
+    print("November, 30 days")
+elif m == l:
+    print("December, 31 days")
+else:
+    print("This is not a number of a month")
